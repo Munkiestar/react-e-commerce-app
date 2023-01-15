b7f18c38f870d710d2864f064a974b0d89a6c0ae
Igor <munkieryder@gmail.com>
2023-01-15 21:12:07 +0100

    add: Product page

--- frontend/src/pages/Product/Product.jsx
+++ frontend/src/pages/Product/Product.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Product.scss";
+
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
+
+const data = [
+  "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+];
 
 function Product(props) {
-  return <div>Product</div>;
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="imgs">
+          <img src={data[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={data[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={data[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>title</h1>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+          consequatur debitis esse fugit iure magnam officiis. Assumenda,
+          consectetur corporis dolores enim expedita molestias provident quae
+          quidem repellat sequi sint temporibus!
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() =>
+              setQuantity((prevState) => (prevState === 1 ? 1 : prevState - 1))
+            }
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prevState) => prevState + 1)}>
+            +
+          </button>
+        </div>
+
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="link">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>{" "}
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
